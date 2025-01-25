8151c80a9fabb284bcd74caa2f4079b593370537
atong005 <angel.tong006@msd.govt.nz>
2025-01-25 14:08:30 +1300

    response

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,4 +37,5 @@ session = cnx.session()
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
