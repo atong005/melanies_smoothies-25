b929e157633ae5d2c6f3a07aab9a662209db9c75
atong005 <angel.tong006@msd.govt.nz>
2025-01-25 17:33:18 +1300

    ll

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,6 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 # my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
 editable_df = st.data_editor(my_dataframe)
 
